6fdd1079c6d708ddb6e1d93a1f6f313218c32628
marcosbeccar <beccarvarelam@udesa.edu.ar>
2024-11-15 14:36:04 -0300

    a nice fresh start

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Login from "./src/screens/Login";
+import Register from "./src/screens/Register";
+import HomeMenu from "./src/screens/HomeMenu";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator(); 
+
+export default function App() { 
+  return (
+    <NavigationContainer> 
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="HomeMenu" component={HomeMenu} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator> 
+    </NavigationContainer> 
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+  },
+});
